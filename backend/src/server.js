13eefe6a9b4baba23677e830df64aee7ae60ecb8
Eshuredd <eshu.maddikunta@gmail.com>
2024-11-16 21:23:43 +0530

    try to host

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,8 +8,7 @@ const quizzesRouter = require('./routes/quizzes');
 const usersRouter = require('./routes/users');
 const adminsRouter = require('./routes/admins');
 const { execFile } = require('child_process');
-const path = require('path');
-
+import path from 'path';
 // Load environment variables from .env file
 dotenv.config();
 
@@ -20,6 +19,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 app.use(express.json());
+const __dirname = path.resolve();
 
 // MongoDB connection
 console.log("MONGO_DB_URL:", process.env.DB_URL)
@@ -31,11 +31,18 @@ mongoose.connect(process.env.DB_URL
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("MongoDB connection error:", err));
 
+
 // Define the API endpoint for posts and quizzes
 app.use('/api/posts', postsRouter);
 app.use('/api/quizzes', quizzesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/admins', adminsRouter);
+
+app.use(express.static(path, join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'App.js'));
+})
 
 app.post('/api/chat', (req, res) => {
     const text = req.body.text;
@@ -93,8 +100,8 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong!');
 });
 
- //Start the server
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-  });
+//Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
