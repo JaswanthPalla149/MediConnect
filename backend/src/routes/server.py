87c153f21e7c485ee15414679497b186536f21a3
JaswanthPalla149 <pallajaswanthroyal@gmail.com>
2024-11-17 15:10:20 +0530

    Another try 4

--- backend/src/routes/server.py
+++ backend/src/routes/server.py
@@ -3,23 +3,23 @@ import sys
 import os
 
 # Determine the path to requirements.txt
-requirements_path = os.path.join(os.getcwd(), "requirements.txt")
-print(requirements_path)
-pip_install = None
+#requirements_path = os.path.join(os.getcwd(), "requirements.txt")
+#print(requirements_path)
+#pip_install = None
 # Install dependencies dynamically
-if os.path.exists(requirements_path):
-    print(f"Installing dependencies from {requirements_path}...")
-   # Ensure that pip install output is suppressed
-    pip_install = subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', requirements_path],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-else:
-    print(f"Error: {requirements_path} does not exist. Please ensure it is in the correct directory.")
+#if os.path.exists(requirements_path):
+#    print(f"Installing dependencies from {requirements_path}...")
+#   # Ensure that pip install output is suppressed
+#    pip_install = subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', requirements_path],
+#                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#else:
+#    print(f"Error: {requirements_path} does not exist. Please ensure it is in the correct directory.")
 
 # Check for errors in pip installation
-if pip_install.returncode != 0:
-    error_message = {"error": f"Error installing requirements: {pip_install.stderr.decode()}"}
-    print(json.dumps(error_message))
-    sys.exit(1)  # Exit if installation fails
+#if pip_install.returncode != 0:
+#    error_message = {"error": f"Error installing requirements: {pip_install.stderr.decode()}"}
+#    print(json.dumps(error_message))
+#    sys.exit(1)  # Exit if installation fails
 
 #import sys
 import json
